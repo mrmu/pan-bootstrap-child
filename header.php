7a71a07895fffe7aa4a63eae3c93636a846b2e20
Audi Lu <khl0327@gmail.com>
2021-06-23 15:55:50 +0800

    remove title

--- header.php
+++ header.php
@@ -2,8 +2,6 @@
 <html <?php language_attributes(); ?> class="no-js">
 	<head>
 		<meta charset="<?php bloginfo('charset'); ?>">
-		<title><?php wp_title(''); ?></title>
-
 		<link href="//www.google-analytics.com" rel="dns-prefetch">
 		<link href="<?php echo VB_THEME_URI; ?>logo.png" rel="shortcut icon">
 		<link href="<?php echo VB_THEME_URI; ?>logo.png" rel="apple-touch-icon-precomposed">
@@ -12,12 +10,13 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<?php wp_head(); ?>
-
 	</head>
 	<body <?php body_class(); ?>>
 		<?php
 		if ( function_exists( 'wp_body_open' ) ) {
 			wp_body_open();
+		} else {
+			do_action( 'wp_body_open' );
 		}
 		?>
 		<!-- header -->
